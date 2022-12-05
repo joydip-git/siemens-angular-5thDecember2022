adc3e0f73b9738de27e6bec76a128aa03db94491
joydip mondal <joydip.mondal@gmail.com>
2022-12-05 12:52:41 +0530

    day-1 pre-lunch update

--- codes/day-1/js-app/index.js
+++ codes/day-1/js-app/index.js
@@ -1,3 +1,24 @@
 function clicked() {
-    console.log('hello world')
+    //console.log('hello world')
+    window.alert('hello world')
+    var spanObj = document.createElement('span')
+    spanObj.innerText = 'hello world'
+
+    var divObj = document.createElement('div')
+    divObj.style.textAlign = 'center'
+    divObj.appendChild(spanObj)
+
+    var brObj = document.createElement('br')
+    //document.body.appendChild(divObj)
+    document.body.append(brObj, divObj)
+
+    /**
+     * <body>
+     * .......
+     * <br>
+     * <div>
+     *  <span>hello world</span>
+     * </div>
+     * </body>
+     */
 }
